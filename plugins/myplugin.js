9520d6d1d5e2e0bd3f59f89722c52367feb689ec
Seshu Koorella <Seshu.Koorella@docscorp.com>
2017-03-07 17:40:37 +1100

    added my simple plugin

---
+++ plugins/myplugin.js
@@ -0,0 +1,73 @@
+// Plugin which registers mail received to a certain address
+// and extracts a From: address from the mail and puts that address
+// in the mail_from.blocklist file. You need to be running the
+// mail_from.blocklist plugin for this to work fully.
+
+var fs = require('fs');
+var utils = require('haraka-utils');
+
+exports.hook_data = function (next, connection) {
+    // enable mail body parsing
+    connection.transaction.parse_body = 1;
+    next();
+}
+
+exports.hook_queue = function (next, connection) {
+
+    connection.loginfo("************** inside queue ********", this);
+
+    next();
+
+};
+
+
+exports.hook_data_post = function (next, connection) {
+
+    var self = this;
+
+    // Make sure only 1 recipient
+    //if (connection.transaction.rcpt_to.length != 1) {
+    //    return next();
+    //}
+
+    self.loginfo('email received to: ' + connection.transaction.rcpt_to[0].address().toLowerCase());
+    // Check recipient is the right one
+    //if (connection.transaction.rcpt_to[0].address().toLowerCase() != recip) {
+    //    return next();
+    //}
+
+    // Check sender is in list
+    var sender = connection.transaction.mail_from.address();
+    //if (!utils.in_array(sender, senders)) {
+    //    return next(DENY, "You are not allowed to block mail, " + sender);
+    //}
+
+    self.loginfo('email received from: ' + sender);
+
+    // Now extract the "From" from the body...
+    //var to_block = extract_from_line(connection.transaction.body);
+    //if (!to_block) {
+    //    connection.logerror(this, "No sender found in email");
+    //    return next();
+    //}
+
+    next();
+}
+
+
+// Example: From: 	Site Tucano Gold <contato@tucanogold.com.br>
+/*function extract_from_line (body) {
+    var matches = body.bodytext.match(/\bFrom:[^<\n]*<([^>\n]*)>/);
+    if (matches) {
+        return matches[1];
+    }
+
+    for (var i=0,l=body.children.length; i < l; i++) {
+        var from = extract_from_line(body.children[i]);
+        if (from) {
+            return from;
+        }
+    }
+
+    return null;
+}*/
